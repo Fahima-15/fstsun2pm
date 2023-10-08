4f59ef262d3de398a358e9d9370f6cc43ea7cc23
Fahima Moalim <moalimfahima15@gmail.com>
2023-10-08 14:49:59 +0100

    first commit

---
+++ loops.js
@@ -0,0 +1,18 @@
+
+//while loops
+
+let i = 0;
+while (i<20) {
+    console.log("FM")
+    i++;
+}
+
+
+
+
+
+
+for (let index = 0; index < 20; index++) {
+    const element = array[Fahima];
+    
+}
